fe0587e0d3283baa89ba0ba44c99b98e4f4b99fd
taromn <54003207+taromn@users.noreply.github.com>
2024-10-15 00:01:50 +0000

    temporary save

--- main.go
+++ main.go
@@ -18,19 +18,45 @@ func main() {
 	defer db.Close()
 
 	// select database names
-	rows, err := db.Query("SELECT datname FROM pg_database WHERE datistemplate = false;")
+	db_rows, err := db.Query("SELECT datname FROM pg_database WHERE datistemplate = false;")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
+	defer db_rows.Close()
+
+	var dbs []string
 
 	// show rows
-	for rows.Next() {
+	for db_rows.Next() {
 		var datname string
-		err = rows.Scan(&datname)
+		err = db_rows.Scan(&datname)
 		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println(datname)
+		dbs = append(dbs, datname)
+	}
+	fmt.Println(dbs)
+
+	// connect to each DB and get schemas
+	for _, each_dbname := range dbs {
+		fmt.Println(each_dbname)
+		each_dsn := dsn + fmt.Sprintf("dbname=%s", each_dbname)
+		each_db, err := sql.Open("postgres", each_dsn)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer each_db.Close()
+
+		schema_rows, err := each_db.Query("select nspname from pg_namespace;")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer schema_rows.Close()
+
+		// after writing all processes,
+		// normalize the func creating connections
+		// normalize the func of query results -> slices
+
 	}
 }
