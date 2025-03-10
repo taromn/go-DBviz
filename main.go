224f33c308d40d40d4e11da9f7e5b828d818ae0f
taromn <54003207+taromn@users.noreply.github.com>
2025-03-10 04:32:11 +0000

    add schema collection

--- main.go
+++ main.go
@@ -9,6 +9,15 @@ import (
 	_ "github.com/lib/pq"
 )
 
+type Schema struct {
+	Sname string
+}
+
+type Db struct {
+	DBname     string
+	SchemaList []Schema
+}
+
 func main() {
 	dsn := os.Getenv("DSN")
 	db, err := sql.Open("postgres", dsn)
@@ -24,39 +33,61 @@ func main() {
 	}
 	defer db_rows.Close()
 
-	var dbs []string
+	var dbs []Db
+	var datname string
 
 	// show rows
 	for db_rows.Next() {
-		var datname string
-		err = db_rows.Scan(&datname)
+		err = db_rows.Scan(&datname) // *database/sql.Rows
 		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println(datname)
-		dbs = append(dbs, datname)
+		dbs = append(dbs, Db{DBname: datname})
 	}
 	fmt.Println(dbs)
 
 	// connect to each DB and get schemas
-	for _, each_dbname := range dbs {
+	for i := range dbs {
+		each_dbname := dbs[i].DBname
 		fmt.Println(each_dbname)
-		each_dsn := dsn + fmt.Sprintf("dbname=%s", each_dbname)
-		each_db, err := sql.Open("postgres", each_dsn)
+		each_dsn := dsn + fmt.Sprintf(" dbname=%s", each_dbname)
+		db_conn, err := sql.Open("postgres", each_dsn)
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
+			continue
 		}
-		defer each_db.Close()
+		defer db_conn.Close()
 
-		schema_rows, err := each_db.Query("select nspname from pg_namespace;")
+		schema_rows, err := db_conn.Query("select nspname from pg_namespace;")
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
+			continue
 		}
 		defer schema_rows.Close()
+
+		var schemas []Schema
+		var schema_name string
+
+		// show schema rows
+		for schema_rows.Next() {
+			err = schema_rows.Scan(&schema_name)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(schema_name)
+			s := Schema{Sname: schema_name}
+			schemas = append(schemas, s)
+		}
+		fmt.Println(schemas)
+
+		dbs[i].SchemaList = schemas
+
+		fmt.Println("final dbs are", dbs)
 
 		// after writing all processes,
 		// normalize the func creating connections
 		// normalize the func of query results -> slices
-
 	}
+
 }
